be3f85c9b4545e159148bd9b450dc764490d609a
manapixels <chrenelias@outlook.com>
2023-08-10 13:54:23 +0800

    Add contracts and token contract addresses

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,6 +1,4 @@
-import { Chains } from "../types/Chains";
-
-export const chains: Chains = {
+export const Contracts = {
    1: {
       name: 'Ethereum',
       logo: '/chains/ethereum.svg',
@@ -16,14 +14,21 @@ export const chains: Chains = {
    10: {
       name: 'Optimism',
       logo: '/chains/optimism.svg',
+      USDT: '0x94b008aa00579c1307b0ef2c499ad98a8ce58e58',
+      USDC: '0x7f5c764cbc14f9669b88837ca1490cca17c31607',
    },
    8453: {
       name: 'Base',
       logo: '/chains/base.svg',
+      USDC: '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA',
+      DAI: '0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb',
    },
    84531: {
       name: 'Base Goerli Testnet',
       logo: '/chains/base.svg',
+      USDT: '0x6440c59d7c7c108d3bb90e4bdeee8262c975858a',
+      USDC: '0xb37a5498a6386b253fc30863a41175c3f9c0723b',
+      USDC2: '0x2e9f75df8839ff192da27e977cd154fd1eae03cf',
    },
    7777777: {
       name: 'Zora',
