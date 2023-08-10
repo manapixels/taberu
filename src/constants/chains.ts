1e192540de18d5182aaeef0c7b537e5a7ac6d979
manapixels <chrenelias@outlook.com>
2023-08-10 15:02:22 +0800

    Update token contract addresses

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,18 +1,6 @@
 import { Chains } from "../types/Chains";
 
 export const chains: Chains = {
-   1: {
-      name: 'Ethereum',
-      logo: '/chains/ethereum.svg',
-   },
-   324: {
-      name: 'zksync Era',
-      logo: '/chains/zksync.svg',
-   },
-   42161: {
-      name: 'Arbitrum',
-      logo: '/chains/arbitrum.svg',
-   },
    10: {
       name: 'Optimism',
       logo: '/chains/optimism.svg',
--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -10,23 +10,13 @@ export type ContractsType = {
 }
 
 export const contracts: ContractsType = {
-   1: {
-      name: 'Ethereum',
-      logo: '/chains/ethereum.svg',
-   },
-   324: {
-      name: 'zksync Era',
-      logo: '/chains/zksync.svg',
-   },
-   42161: {
-      name: 'Arbitrum',
-      logo: '/chains/arbitrum.svg',
-   },
    10: {
+      // https://github.com/ethereum-optimism/ethereum-optimism.github.io/blob/master/data/USDC/data.json
       name: 'Optimism',
       logo: '/chains/optimism.svg',
-      USDT: '0x94b008aa00579c1307b0ef2c499ad98a8ce58e58',
+      USDT: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58',
       USDC: '0x7f5c764cbc14f9669b88837ca1490cca17c31607',
+      DAI: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
    },
    8453: {
       name: 'Base',
@@ -38,8 +28,10 @@ export const contracts: ContractsType = {
       name: 'Base Goerli Testnet',
       logo: '/chains/base.svg',
       USDT: '0x6440c59d7c7c108d3bb90e4bdeee8262c975858a',
-      USDC: '0xb37a5498a6386b253fc30863a41175c3f9c0723b',
-      USDC2: '0x2e9f75df8839ff192da27e977cd154fd1eae03cf',
+      USDC: '0x853154e2A5604E5C74a2546E2871Ad44932eB92C',
+      //   USDC: '0xb37a5498a6386b253fc30863a41175c3f9c0723b',
+      //   USDC: '0x2e9f75df8839ff192da27e977cd154fd1eae03cf',
+      DAI: '0x174956bDfbCEb6e53089297cce4fE2825E58d92C',
    },
    7777777: {
       name: 'Zora',
