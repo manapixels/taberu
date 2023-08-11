5b730502dcaa1da709b6f1ee910d8ecdbeeb88e8
manapixels <chrenelias@outlook.com>
2023-08-11 22:34:46 +0800

    Fix contract's redemption value

--- src/contracts/LoyaltyProgram.sol
+++ src/contracts/LoyaltyProgram.sol
@@ -7,14 +7,16 @@ contract LoyaltyProgram {
     // The address of the shop owner
     address public owner;
 
-    // The amount of ETH required to get one stamp
-    uint256 public stampPrice;
+    // The amount of ETH (in Wei) required to get one point
+    uint256 public pointPrice;
+    uint256 public pointValue;
 
-    // A mapping of customer addresses to their stamp balances
-    mapping(address => uint256) public stamps;
+    // A mapping of customer addresses to their point balances
+    mapping(address => uint256) public points;
 
-    event StampsAwarded(address indexed customer, uint256 amount);
-    event StampsRedeemed(address indexed customer, uint256 amount);
+    event Paid(address indexed customer, uint256 amount);
+    event PointsAwarded(address indexed customer, uint256 amount);
+    event PointsRedeemed(address indexed customer, uint256 amount);
 
     // A modifier to check that only the owner can call a function
     modifier onlyOwner() {
@@ -22,28 +24,32 @@ contract LoyaltyProgram {
         _;
     }
 
-    // The constructor that sets the stamp price
-    constructor(uint256 _stampPrice) {
-        stampPrice = _stampPrice;
+    // The constructor that sets the point price
+    constructor(uint256 _pointPrice, uint256 _pointValue) {
+        pointPrice = _pointPrice;
+        pointValue = _pointValue;
         owner = msg.sender;
     }
 
-    // A function that allows customers to pay in ETH and get stamps
+    // A function that allows customers to pay in ETH and get points
     function payWithETH() external payable {
         require(msg.value > 0, "Amount must be positive");
-        uint256 _stamps = msg.value / stampPrice;
-        stamps[msg.sender] += _stamps;
-        emit StampsAwarded(msg.sender, _stamps);
+        uint256 _points = msg.value / pointPrice;
+        points[msg.sender] += _points;
+        
+        emit PointsAwarded(msg.sender, _points);
+        emit Paid(msg.sender, msg.value);
     }
 
-    // A function that allows customers to redeem their stamps for ETH
-    function redeemStamps(uint256 _amount) external {
+    // A function that allows customers to redeem their points for ETH
+    function redeemPoints(uint256 _amount) external {
         require(_amount > 0, "Amount must be positive");
-        require(stamps[msg.sender] >= _amount, "Not enough stamps");
-        uint256 _eth = _amount * stampPrice;
+        require(points[msg.sender] >= _amount, "Not enough points");
+        uint256 _eth = _amount * pointValue;
         payable(msg.sender).transfer(_eth);
-        stamps[msg.sender] -= _amount;
-        emit StampsRedeemed(msg.sender, _amount);
+        points[msg.sender] -= _amount;
+        
+        emit PointsRedeemed(msg.sender, _amount);
     }
 
     // A function that allows the owner to withdraw the ETH balance of the contract
